0a9c96822161f98b2baf03b3d41d538b4c3b887c
codewithlyawile <webdev271@gmail.com>
2023-01-12 17:37:19 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var x = 100
+    var age = 8
+    var firstName = "Juma"
+    var lastName = "Lyawile"
+    var isTall = true
+
+    println(x)
+    println(age)
+    print(firstName)
+    println(lastName)
+}
